9b8e3561ef47325aa990367525ac1a3ae86f15b1
bhowey1988 <ben_howey@rnli.org.uk>
2024-08-06 11:43:47 +0100

    logoutput to db and changed sqlite connector

--- connectors/sqlite_connector.py
+++ connectors/sqlite_connector.py
@@ -5,6 +5,27 @@ import requests
 import tempfile
 import os
 
+
+def create_db(path, headers_list, db_path):
+    if not os.path.exists(db_path):
+        conn = sqlite3.connect(db_path)
+        c = conn.cursor()
+        c.execute(f'CREATE TABLE IF NOT EXISTS Incident ({headers_list})')
+
+        with open(path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # Skip the header row
+            for row in reader:
+                placeholders = ",".join(["?"] * len(row))
+                sql = f'INSERT INTO Incident VALUES ({placeholders})'
+                c.execute(sql, row)
+
+        conn.commit()
+        conn.close()
+    else:
+        print('Database already exists')
+
+
 HEADERS = {"OBJECTID": {"dtype": "PRIMARY KEY", "description": "Unique ID of the incident"},
          "ROSNumber": {"dtype": "TEXT", "description": "The coast guard Incident number"},
          "LifeboatStationNameProper": {"dtype": "TEXT", "description": "The name of the responding lifeboat station"},
@@ -34,55 +55,36 @@ HEADERS = {"OBJECTID": {"dtype": "PRIMARY KEY", "description": "Unique ID of the
          }
 
 
+# create the database here so when it gets imported it runs and creates one instance of the db
+DB_PATH = os.path.join(os.path.dirname(__file__), 'RNLI_Return_of_Service.db')
+req = requests.get('https://opendata.arcgis.com/api/v3/datasets/7fe0801fbf6147cb8053050316f15631_1/downloads/data?format=csv&spatialRefId=4326&where=1%3D1')
+# Check if the request was successful
+csv_path = os.path.join(os.path.dirname(__file__), 'RNLI_Return_of_Service.csv')
+if req.status_code == 200:
+    with open(csv_path, 'wb') as file:
+        file.write(req.content)
+
+headers_dtype_list = [f"{key} {value['dtype']}" for key, value in HEADERS.items()]
+headers_list = ", ".join(headers_dtype_list)
+headers_description_list = [f"{key} {value['description']}" for key, value in HEADERS.items()]
+headers_description_list = ", ".join(headers_description_list)
+create_db(csv_path, headers_list, DB_PATH)
+
+
 class SQLliteConnector():
     def __init__(self):
-        # download data locally and convert to sqlite
-        req = requests.get('https://opendata.arcgis.com/api/v3/datasets/7fe0801fbf6147cb8053050316f15631_1/downloads/data?format=csv&spatialRefId=4326&where=1%3D1')
-        # Check if the request was successful
-        if req.status_code == 200:
-            # make a temp dir
-            temp_dir = tempfile.mkdtemp()
-            # Save the content to a local file
-            with open(os.path.join(temp_dir, 'RNLI_Return_of_Service.csv'), 'wb') as file:
-                file.write(req.content)
+        self.database_path = DB_PATH
 
-            headers_dtype_list=[f"{key} {value['dtype']}" for key, value in HEADERS.items()]
-            self.headers_list = ", ".join(headers_dtype_list)
+        self.table_name = 'Incident'
 
-            # generate the headers description list
-            headers_description_list = [f"{key} {value['description']}" for key, value in HEADERS.items()]
-            self.headers_description_list = ", ".join(headers_description_list)
+        self.database_type = 'sqlite'
 
-            self.table_name = 'Incident'
+        self.additional_system_info = 'When filtering on station names or locations always use case insensitive fuzzy matching'
 
-            self.database_type = 'sqlite'
-
-            self.additional_system_info = ''
-
-            # create a db from the data
-            self.create_db(os.path.join(temp_dir,'RNLI_Return_of_Service.csv'), self.headers_list)
-
-    def create_db(self, path, headers_list):
-        db_path = path.replace('.csv', '.db')
-        conn = sqlite3.connect(db_path)
-        c = conn.cursor()
-        c.execute(f'CREATE TABLE IF NOT EXISTS {self.table_name} ({headers_list})')
-
-        with open(path, 'r') as f:
-            reader = csv.reader(f)
-            next(reader)  # Skip the header row
-            for row in reader:
-                placeholders = ",".join(["?"] * len(row))
-                sql = f'INSERT INTO Incident VALUES ({placeholders})'
-                c.execute(sql, row)
-
-        conn.commit()
-        conn.close()
-
-        self.databse_path = db_path
+        self.headers_description_list = headers_description_list
 
     def query_source_data(self, query):
-        conn = sql.connect(self.databse_path)
+        conn = sql.connect(self.database_path)
         c = conn.cursor()
         c.execute(query)
         result = c.fetchall()
